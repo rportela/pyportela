f7dd69d206f4db88c45c52d72ba51ab087f30e6c
Rodrigo Portela <rodrigo.portela@gmail.com>
2024-09-06 00:40:53 -0300

    rodobens needs data log

--- pyportela/services/SupabaseClient.py
+++ pyportela/services/SupabaseClient.py
@@ -1,30 +1,16 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from typing import Optional
+
 from supabase import Client, create_client
-
-
-class RedirectHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Parse the query parameters
-        path = self.path.split("?")[1] if "?" in self.path else ""
-        query_params = dict(p.split("=") for p in path.split("&"))
-        print("Received query params:", query_params)
-
-        # Here, you can extract the token and do whatever you need with it
-
-        # Send a response back to the client
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Google sign-in successful! You can close this window.")
+from supabase.lib.client_options import ClientOptions
 
 
 class SupabaseClient:
 
     supabase: Client
 
-    def __init__(self) -> None:
+    def __init__(self, api_key: str) -> None:
         url: str = os.environ.get(
             "SUPABASE_URL", "https://cuaygngchxpwxaozhzro.supabase.co"
         )
@@ -32,22 +18,9 @@ class SupabaseClient:
             "SUPABASE_KEY",
             "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1YXlnbmdjaHhwd3hhb3poenJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU1NTYwMjgsImV4cCI6MjAwMTEzMjAyOH0.Yp4u8akcsxJGgLWt5uRshFw3KanwXiDZpVZrttUKobI",
         )
-        self.supabase = create_client(url, key)
-
-    supabaseClient: Optional["SupabaseClient"] = None
-    httpd: Optional[HTTPServer] = None
-
-    @staticmethod
-    def get_instance() -> "SupabaseClient":
-        if SupabaseClient.supabaseClient is None:
-            SupabaseClient.supabaseClient = SupabaseClient()
-        return SupabaseClient.supabaseClient
+        self.supabase = create_client(
+            url, key, ClientOptions(headers={"apikey": api_key})
+        )
 
     def table(self, name: str):
         return self.supabase.table(name)
-
-    def start_server(self):
-        server_address = ("", 8888)
-        SupabaseClient.httpd = HTTPServer(server_address, RedirectHandler)
-        print("Starting server at http://localhost:8888")
-        SupabaseClient.httpd.serve_forever()
